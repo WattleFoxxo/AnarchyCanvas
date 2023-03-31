5bd251b1adc601b677e98e563010a73af54ce6b8
WattleFoxxo <88825700+WattleFoxxo@users.noreply.github.com>
2023-03-31 11:49:40 +1100

    first

---
+++ public/js/app.js
@@ -0,0 +1,80 @@
+//---------------------------//
+//  app.js - Anarchy Canvas  //
+//---------------------------// 
+
+var config = JSON.parse(httpGet("http://localhost:3000/api/config"));
+
+var socket = io();
+
+var pickedColour = 1;
+
+let type = "WebGL";
+if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas";
+}
+
+PIXI.utils.sayHello(type);
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+const app = new PIXI.Application({ 
+    width: window.innerWidth,
+    height: window.innerHeight,
+    resizeTo: window,
+    antialias: false,
+    transparent: false,
+    resolution: 1
+});
+
+document.body.appendChild(app.view);
+app.view.oncontextmenu = function(e) { e.preventDefault(); e.stopPropagation(); }
+app.renderer.backgroundColor = 0xc1c1c2;
+
+const viewport = new pixi_viewport.Viewport({
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight,
+    worldWidth: config.width,
+    worldHeight: config.height,
+    events: app.renderer.events
+})
+app.stage.addChild(viewport)
+
+viewport.drag({"mouseButtons":"middle-right"}).pinch().wheel().decelerate()
+viewport.fit()
+viewport.moveCenter(config.width / 2, config.height / 2)
+viewport.zoom
+viewport.clampZoom({"minScale":0.5, "maxScale":15})
+
+viewport.on("clicked", (e) => {
+    if ((e.world.x >= 0 && e.world.y >= 0) && (e.world.x <= config.width && e.world.y <= config.height)){
+        socket.emit("draw", {"colour":pickedColour, "x":e.world.x, "y":e.world.y})
+        console.log('clicked (' + e.world.x + ',' + e.world.y + ')')
+    }
+});
+
+var texture = PIXI.Texture.from('http://localhost:3000/api/canvas');
+var canvasSprite = new PIXI.Sprite(texture);
+viewport.addChild(canvasSprite);
+
+socket.on('server_draw', (msg) => {
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(config.colours[msg.colour]);
+    graphics.drawRect(parseInt(msg.x), parseInt(msg.y), 1, 1);
+    graphics.endFill();
+    viewport.addChild(graphics);
+});
+
+
+function setColour(colour) {
+    var oldbutton = document.getElementById(pickedColour);
+    oldbutton.classList = "active";
+    var button = document.getElementById(colour);
+    button.classList = "active focused";
+    pickedColour = colour;
+}
+
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+// webserver
+const express = require('express')
+const app = express()
+const http = require('http');
+const server = http.createServer(app);
+
+// image
+const Jimp = require("jimp");
+
+// sockets
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// config
+const config = require('./config.json')
+
+var canvas;
+
+// home page
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+"\\public\\index.html")
+})
+
+// sockets
+io.on("connection", (socket) => {
+
+    socket.on("draw", (msg) => {
+        canvas.setPixelColor(parseInt(config.canvas.colours[msg.colour]+"FF"), parseInt(msg.x), parseInt(msg.y));
+        socket.broadcast.emit("server_draw", msg);
+        socket.emit("server_draw", msg);
+    });
+
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+app.get('/api/canvas', function (req, res) {
+    canvas.write(config.canvasPath)
+    res.sendFile(config.canvasPath)
+})
+
+app.get('/api/config', function (req, res) {
+    res.send(JSON.stringify(config.canvas));
+})
+
+app.use(express.static('public'))
+
+
+Jimp.read(config.canvasPath, (err, image) => {
+    if (err) throw err;
+    canvas = image
+});
+
+
+server.listen(config.port, () => {
+    console.log(`listening on *:${config.port}`);
+});
